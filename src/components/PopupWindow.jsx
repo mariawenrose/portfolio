b18e93ef338613abfdc3097f80b2ccb757177501
mariawenrose <mariarose.wen@gmail.com>
2020-04-28 17:46:18 +1200

    sketch tree

---
+++ src/components/PopupWindow.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+export default class PopupWindow extends Component {
+    render() {
+        return (
+            <p>im the window</p>
+        )
+    }
+}
