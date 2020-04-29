601f874704db9d83fed315ab123995e7de5d06de
mariawenrose <mariarose.wen@gmail.com>
2020-04-29 14:07:29 +1200

    added working timer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,31 @@
 
-import React, {Component} from 'react'
+import React, { Component } from 'react'
 
 export default class Footer extends Component {
-    render() {
-        return (
-            <div>
-                <h2>hi</h2>
-            </div>
-        )
+  constructor() {
+    super()
+    this.state = {
+      time: new Date()
     }
+  }
+
+  currentTime() {
+    this.setState({
+      time: new Date()
+    })
+  }
+  componentWillMount() {
+    setInterval(() => this.currentTime(), 1000)
+  }
+
+
+  render() {
+    return (
+      <div className="start-bar">
+        <div id="clock">
+          <h1>{this.state.time.toLocaleTimeString()}</h1>
+        </div>
+      </div>
+    )
+  }
 }
