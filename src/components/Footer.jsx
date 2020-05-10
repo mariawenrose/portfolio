fc8dbed21e7d5f539078fe6b731c06d8be157de6
mariawenrose <mariarose.wen@gmail.com>
2020-05-10 19:54:23 +1200

    added time framework

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,29 +1,48 @@
 
 import React, { Component } from 'react'
+import { format } from 'date-fns'
+
+
+const formatCurrentTime = () => format(new Date(), "HH:mm");
+
 
 export default class Footer extends Component {
+
+  intervalId = undefined
+
+
   constructor() {
     super()
     this.state = {
-      time: new Date()
+      time: formatCurrentTime()
     }
   }
 
   currentTime() {
     this.setState({
-      time: new Date()
+      time: formatCurrentTime()
     })
   }
-  componentWillMount() {
-    setInterval(() => this.currentTime(), 1000)
+
+  shouldComponentUpdate(_nextProps, nextState) {
+    const currentState = this.state
+    return currentState.time !== nextState.Time;
   }
 
+  componentDidMount() {
+    this.intervalId = setInterval(() => this.currentTime(), 1000)
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.inervalId);
+    this.intervalId = undefined
+  }
 
   render() {
     return (
       <div className="start-bar">
         <div id="clock">
-          <p>{this.state.time.toLocaleTimeString()}</p>
+          <p>{this.state.time}</p>
         </div>
       </div>
     )
